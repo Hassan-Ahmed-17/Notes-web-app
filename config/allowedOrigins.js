b9cd3d9a0c413e5a76b3f4a6c664d878e89c8cbf
Hassan-Ahmed-17 <hassan1717@gmail.com>
2023-03-14 19:37:24 +0500

    backend

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+//list of allowed urls that can access our rest api
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
+]
+
+module.exports = allowedOrigins
